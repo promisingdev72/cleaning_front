0e2bc2ee8ffabc8153db19507bd25bb7bc511de0
promisingdev72 <promising.dev72@gmail.com>
2022-05-12 08:43:44 +0300

    update

--- src/layouts/main/index.js
+++ src/layouts/main/index.js
@@ -4,7 +4,6 @@ import { Box, Link, Container, Typography, Stack } from '@mui/material';
 // components
 import Logo from '../../components/Logo';
 //
-import MainFooter from './MainFooter';
 import MainHeader from './MainHeader';
 
 // ----------------------------------------------------------------------
@@ -21,29 +20,21 @@ export default function MainLayout() {
       <Outlet />
 
       <Box sx={{ flexGrow: 1 }} />
-
-      {!isHome ? (
-        <MainFooter />
-      ) : (
-        <Box
-          sx={{
-            py: 5,
-            textAlign: 'center',
-            position: 'relative',
-            bgcolor: 'background.default',
-          }}
-        >
-          <Container>
-            <Logo sx={{ mb: 1, mx: 'auto' }} />
-
-            <Typography variant="caption" component="p">
-              © All rights reserved
-              <br /> made by &nbsp;
-              <Link href="https://minimals.cc/">minimals.cc</Link>
-            </Typography>
-          </Container>
-        </Box>
-      )}
+      <Box
+        sx={{
+          py: 5,
+          textAlign: 'center',
+          position: 'relative',
+          bgcolor: 'background.default',
+        }}
+      >
+        <Container>
+          <Logo sx={{ mb: 1, mx: 'auto' }} />
+          <Typography variant="caption" component="p">
+            <Link href="#">admin@admin.com</Link>
+          </Typography>
+        </Container>
+      </Box>
     </Stack>
   );
 }
